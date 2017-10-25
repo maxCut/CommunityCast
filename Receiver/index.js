14ebe1137dfcbb7badbaf0dbf493e720a1d5fcec
max7098 <max.mesirow@gmail.com>
2017-10-24 22:45:03 -0400

    moved files to fix structure

--- Receiver/index.js
+++ Receiver/index.js
@@ -3,9 +3,10 @@ var app = express();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '../index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 http.listen((process.env.PORT || 3000), function(){
   console.log('listening on *:3000');
 });
+
