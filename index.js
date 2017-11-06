d44232ee2755e24ba41ec67138956e0417c239e2
max7098 <max.mesirow@gmail.com>
2017-11-05 20:42:00 -0500

    coppied the files from the receiver application

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen((process.env.PORT || 3000), function(){
+  console.log('listening on *:3000');
+});
+
