939e940999ce35abc8dedb2fcab58fd88442df4b
max7098 <max.mesirow@gmail.com>
2017-11-12 21:41:43 -0500

    added more to the connection handleing

--- public/communityCastMain.js
+++ public/communityCastMain.js
@@ -1,12 +1,28 @@
-'use strict';
-//The interface that the sender applications will communicate with
-var communityInterface = null;
+var cast = cast || {}; cast.games = cast.games || {};
+cast.games.common = {};
+cast.games.common.receiver = {};
+cast.games.common.receiver.Game = function() {
+};
+cast.games.spritedemo = {};
+cast.games.spritedemo.SpritedemoMessageType = {UNKNOWN:0, SPRITE:1};
+cast.games.spritedemo.SpritedemoMessage = function() {
+  this.type = cast.games.spritedemo.SpritedemoMessageType.UNKNOWN;
+};
+
+cast.games.spritedemo = {};
+cast.games.spritedemo.SpritedemoMessageType = {UNKNOWN:0, SPRITE:1};
+
+cast.games.spritedemo.SpritedemoGame = function(gameManager) {
+  this.gameManager_ = gameManager;
+};
+
+var game = null;
 
 var initialize = function() {
   var castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
   var appConfig = new cast.receiver.CastReceiverManager.Config();
 
-  appConfig.statusText = 'CommunityCast';
+  appConfig.statusText = 'Spritedemo';
   // In production, use the default maxInactivity instead of using this.
   appConfig.maxInactivity = 6000;
 
@@ -14,10 +30,35 @@ var initialize = function() {
   // cast namespaces can be set up.
   /** @suppress {missingRequire} */
   var gameConfig = new cast.receiver.games.GameManagerConfig();
-  gameConfig.applicationName = 'CommunityCast';
+  gameConfig.applicationName = 'Spritedemo';
   gameConfig.maxPlayers = 10;
+
+  /** @suppress {missingRequire} */
+  var gameManager = new cast.receiver.games.GameManager(gameConfig);
+
+  /** @suppress {missingRequire} */
+  game = new cast.games.spritedemo.SpritedemoGame(gameManager);
+
+  var startGame = function() {
+    game.run(function() {
+      console.log('Game running.');
+      gameManager.updateGameStatusText('Game running.');
+    });
+  };
+
+  castReceiverManager.onReady = function(event) {
+    if (document.readyState === 'complete') {
+      startGame();
+    } else {
+      window.onload = startGame;
+    }
+  };
   castReceiverManager.start(appConfig);
-  game.run(function() {
-    console.log('cast running.');
-  });
 };
+
+if (document.readyState === 'complete') {
+  initialize();
+} else {
+  /** Main entry point. */
+  window.onload = initialize;
+}
