c348dcef8c71c02544d84e37e739918854b7fcfd
Chris Ackerman <caa96@bu.edu>
2017-12-03 17:11:57 -0500

    removed test js

--- public/screenCast.js
+++ public/screenCast.js
@@ -16,7 +16,6 @@ document.getElementById('start').addEventListener('click', function() {
 
 // listen for messages from the content-script
 window.addEventListener('message', function (event) {
-  console.log('js is updated');
   console.log('event data type is ' + event.data.type);
   if (event.origin != window.location.origin) return;
 
